a361aede85935ed84299ae1056bc330361b23e4b
Nguyen Minh Chau <chaunm27@fpt.com>
2024-10-04 10:58:57 +0700

    push code

--- src/Routers/user.js
+++ src/Routers/user.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
-const checkToken = require('../utils/checkToken');
-const checkRole = require('../utils/checkRole');
 const userController = require('../Controllers/userController');
 
 router.post('/create', userController.createUser);
 router.get('/getall', userController.getAllUsers);
 router.get('/:id', userController.getUser);
-router.put('/:id', checkToken, userController.updateUser);
-router.delete('/:id', checkToken, checkRole, userController.deleteUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
--- src/Utils/checkRole.js
+++
@@ -1,9 +0,0 @@
-const checkRole = (req, res, next) => {
-    if (req.user?.id === req.params?.id || req.user?.role === 'admin') next();
-    else
-        return res
-            .status(403)
-            .json({ message: 'You are not allowed to do this!' });
-};
-
-module.exports = checkRole;
